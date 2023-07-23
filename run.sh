aafd5e167ea621ad0255ee9e5b938f9b04c83a8e
radarFudan <25528016+radarFudan@users.noreply.github.com>
2023-07-23 07:55:42 +0000

    prepared dataset

--- run.sh
+++ run.sh
@@ -1,45 +1,45 @@
 python train.py \
---train_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_train.json \
---dev_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_dev.json \
+--train_file /home/aiops/wangsd/github/t5-pegasus-pytorch/LCSTS_new/small_train.json \
+--dev_file /home/aiops/wangsd/github/t5-pegasus-pytorch/LCSTS_new/small_dev.json \
 --batch_size 6 \
 --max_epochs 10 \
 --max_source_length 512 \
 --max_target_length 300 \
---model_path /home/xianglingyang/pretrained_models/torch/t5-copy \
+--model_path imxly/t5-copy \
 --gpus 4 \
 --lr 5e-5 --model_type t5copy
 
 
-python train.py \
---train_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_train.json \
---dev_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_dev.json \
---batch_size 6 \
---max_epochs 10 \
---max_source_length 512 \
---max_target_length 150 \
---model_path /home/xianglingyang/pretrained_models/torch/t5-copy  \
---gpus 4 \
---lr 5e-5 --model_type t5-pegasus
+# python train.py \
+# --train_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_train.json \
+# --dev_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_dev.json \
+# --batch_size 6 \
+# --max_epochs 10 \
+# --max_source_length 512 \
+# --max_target_length 150 \
+# --model_path /home/xianglingyang/pretrained_models/torch/t5-copy  \
+# --gpus 4 \
+# --lr 5e-5 --model_type t5-pegasus
 
 
-python train.py \
---train_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_train.json \
---dev_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_dev.json \
---batch_size 6 \
---max_epochs 10 \
---max_source_length 512 \
---max_target_length 300 \
---model_path /home/xianglingyang/pretrained_models/torch/cpt-large  \
---gpus 4 \
---lr 5e-5 --model_type cpt --rdrop
+# python train.py \
+# --train_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_train.json \
+# --dev_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_dev.json \
+# --batch_size 6 \
+# --max_epochs 10 \
+# --max_source_length 512 \
+# --max_target_length 300 \
+# --model_path /home/xianglingyang/pretrained_models/torch/cpt-large  \
+# --gpus 4 \
+# --lr 5e-5 --model_type cpt --rdrop
 
-python train.py \
---train_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_train.json \
---dev_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_dev.json \
---batch_size 6 \
---max_epochs 10 \
---max_source_length 512 \
---max_target_length 300 \
---model_path /home/xianglingyang/pretrained_models/torch/prophet  \
---gpus 4 \
---lr 5e-5 --model_type prophet
+# python train.py \
+# --train_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_train.json \
+# --dev_file /home/xianglingyang/data/faith_gen/LCSTS_new/small_dev.json \
+# --batch_size 6 \
+# --max_epochs 10 \
+# --max_source_length 512 \
+# --max_target_length 300 \
+# --model_path /home/xianglingyang/pretrained_models/torch/prophet  \
+# --gpus 4 \
+# --lr 5e-5 --model_type prophet
